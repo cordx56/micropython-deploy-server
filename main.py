a69f9f071b3bcae8ff69865d56b3fb50e0f8c1a5
CHISEN Kaoru <cordx56@cordx.net>
2022-09-26 19:13:12 +0900

    support non-SD mode

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from machine import SDCard
 import os
 
-sd = SDCard(slot=2)
-os.mount(sd, "/sd")
+try:
+    sd = SDCard(slot=2)
+    os.mount(sd, "/sd")
+    # "import sd" is fail in MicroPython
+    os.chdir("/sd")
+except:
+    print("SD card not found")
 
-# "import sd" is fail in MicroPython
-os.chdir("/sd")
 with open("__init__.py", "r") as f:
     exec(f.read())
